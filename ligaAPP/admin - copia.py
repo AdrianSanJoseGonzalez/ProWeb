157d16c591131459cf3000b1b11bf90ecee62070
AdrianSanJoseGonzalez <115790325+AdrianSanJoseGonzalez@users.noreply.github.com>
2024-11-05 13:43:35 +0100

    e

--- ligaAPP/admin - copia.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Liga, Equipo, Jugador
-
-# Register your models here.
-admin.site.register(Liga)
-admin.site.register(Equipo)
-admin.site.register(Jugador)
